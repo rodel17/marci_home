d43232e593ba3dca205bf847270965a771ec0fd2
rodel17 <rodelagocoy17@gmail.com>
2025-04-24 19:25:23 +0800

    save progress

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(function() {
   console.log('Script ready');
+
+  $('.open-menu').click(function() {
+    $('.open-menu i').toggle();
+    $('.menu-list-container').fadeToggle();
+    // $('.open-menu i.fa-xmark').fadeToggle();
+  })
 })
